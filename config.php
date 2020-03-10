bbf699456e7af432ffe87aa85dd71a48ea4b1877
Shokhrukh <58341874+nextin94@users.noreply.github.com>
2020-03-10 23:03:43 +0300

    config.php
    phpmyadmin connect to config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'localhost';
+$user = 'ivanov2929_ivan';
+$password = 'Mimikali111';
+$db_name = 'ivanov2929_ivan';
+
+$link  = mysqli_connect($host, $user, $password, $db_name);
+
+?>
