b4e41022af1cd7a83e79ec0a535618880431065c
Shokhrukh <58341874+nextin94@users.noreply.github.com>
2020-03-14 13:52:56 +0300

    Index php
    Даны файлы 1.php, 2.php, 3.php. Сделайте так, чтобы на странице index.php с помощью GET-запроса подключался один из файлов. GET-параметром передавайте имя подключаемого файла вместе с расширением.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$page = $_GET['page'];
+include "$page.php";
+echo '<br>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+<a href="?page=1">first site</a><br>
+<a href="?page=2">second site</a><br>
+<a href="?page=3">third site</a><br>
